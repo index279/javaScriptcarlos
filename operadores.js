07def4052a2e3b94465c55443051a887ec7b6528
index279 <mamiandaniel9@gmail.com>
2025-08-09 09:16:16 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+//suma,resta,multplicacion,division
+//calcular valores de dos o mas elementos
+let num1 = 50;
+var num2 = 19;
+let suma = num1 + num2;
+console.log("el resultado es:" + suma)
+//contadore suma
+let edad = 9;
+edad=edad + 1;//edad++
+console.log("edad es:"+edad);
+//resta
+let res1 = 100;
+var res2 = 31;
+let resta = res1 - res2;
+console.log("la resta es:" + resta)
+//contador resta
+let bombones = 8;
+bombones = bombones - 1;
+console.log("quedan " + bombones + " bombones " );
+//multiplicacion
+let num3 = 3;
+let num4 = 23;
+let multiplicacion = num3 * num4;
+console.log("la multiplicacion es " + multiplicacion );
+//division
+let div1 = 9;
+let div2 = 3;
+let division = div1 / div2 ;
+console.log("la division es " + division);
+//residuo
+let resd1 = 10;
+let resd2 = 3;
+let residuo = res1 % resd2;
+console.log("el resudio es " + residuo);
+
+
---
+++ variables2.js
@@ -0,0 +1,18 @@
+// string
+var sena="ADSO";
+console.log(sena);
+//numeros
+let numeroEntero=17;
+var numeroNegativo=-4;
+let numeroDecimal=11.74;
+//Booleanos true-false
+var estoyEstudiando=true;
+let estoyAburrido=false;
+//undefined
+var num;
+console.log(num);
+num=0;
+console.log(num);
+//null
+var sinNada=null;
+console.log(sinNada);
