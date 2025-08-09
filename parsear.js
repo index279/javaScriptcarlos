5e2a88cdadfa2218982a68c2a72e1b6ece1130c4
index279 <mamiandaniel9@gmail.com>
2025-08-09 11:59:04 -0500

    parsear

---
+++ parsear.js
@@ -0,0 +1,7 @@
+let num1 = prompt("digite el primer numero ");
+let num2 = prompt("digite el segundo numero ");
+ num1 = parseInt(num1);
+ num2 = parseInt(num2)
+let suma = num1 + num2 ;
+console.log(suma);
+alert("la suma es  " + suma);
---
+++ prompt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt("dgite palabra a concatenar ");
+let palabra2 = prompt("dgite palabra 2 a concatenar ");
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);
+alert("esta es mi palabra concatenada " + concatenar);
